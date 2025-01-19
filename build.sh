5a433b3022a18f034c54ea4ffe02963f4f05c5d0
Aayush Goswami <silver.bittu@gmail.com>
2025-01-20 03:04:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ chmod +x download_dll.sh
 
 # Install tesseract_ocr
 sudo apt-get update && sudo apt-get install -y tesseract-ocr
+which tesseract && tesseract --version
 
 # Continue with Python dependencies
 pip install -r requirements.txt
