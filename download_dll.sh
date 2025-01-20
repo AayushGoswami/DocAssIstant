ef16619ab68cd5ce2a92f21020c8d972e85419e7
Aayush Goswami <silver.bittu@gmail.com>
2025-01-20 14:04:40 +0530

    Update download_dll.sh

--- download_dll.sh
+++ download_dll.sh
@@ -5,7 +5,7 @@ mkdir -p utils/OCR/Tesseract-OCR
 
 # Download the file
 
-https://drive.google.com/file/d/1HgxAEzr6SHB8wZpfwX5DBKsgKAoDaZY9/view?usp=sharing
+# https://drive.google.com/file/d/1HgxAEzr6SHB8wZpfwX5DBKsgKAoDaZY9/view?usp=sharing
 
 wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=1HgxAEzr6SHB8wZpfwX5DBKsgKAoDaZY9' -O utils/OCR/Tesseract-OCR/libicudt75.dll
 
