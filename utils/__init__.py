9e104be4a04ca6dc7ec3a03ce5d7a9188b3ee0e3
Aayush Goswami <silver.bittu@gmail.com>
2025-01-23 01:40:38 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,5 @@
 # Import key functions and classes for easy access
 from .pdf_extractor import extract_text_from_pdf, validate_pdf
-from .image_extractor import extract_text_from_image, supported_image_formats
 from .ai_processor import AIProcessor
 
 __all__ = [
@@ -10,3 +9,4 @@ __all__ = [
     'supported_image_formats',
     'AIProcessor'
 ]
+
